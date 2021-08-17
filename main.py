f40922b8386b12883bd7fb470413b9140d77a0f7
Canino <cannencc@gmail.com>
2021-08-16 21:50:32 -0400

    Graphs

--- main.py
+++ main.py
@@ -7,6 +7,17 @@ from keras.models import Sequential
 from keras.layers import LSTM,Dropout,Dense
 from sklearn.preprocessing import MinMaxScaler
 
+def plotShit(foo:str):
+
+    data = pd.read_csv(foo, index_col='Time(ms)')
+    print(data)
+    plt.plot(data)
+    plt.xlabel("Time(ms)")
+    plt.ylabel("Price($)")
+    plt.show()
+
+
 if __name__ == "__main__":
     # execute only if run as a script
     print("Hello world")
+    plotShit("data.csv")
